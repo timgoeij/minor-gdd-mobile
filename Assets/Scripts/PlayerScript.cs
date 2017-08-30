a808056ddcd2e2e653e9a798d46e378c86dd6267
Marcel Dillen <marceldillen@me.com>
2017-08-30 12:56:05 +0200

    Add running player

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	private float _speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate( Vector3.right * _speed );
+	}
+}
