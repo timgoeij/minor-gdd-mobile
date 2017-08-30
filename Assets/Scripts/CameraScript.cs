fe3441284fb525688d0a36406fd52dc32ea04044
Marcel Dillen <marceldillen@me.com>
2017-08-30 16:03:38 +0200

    Fix position for different aspect ratios

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -16,9 +16,24 @@ public class CameraScript : MonoBehaviour {
 		transform.position = Vector3.Lerp(
 			transform.position, 
 			new Vector3(
-				_player.transform.position.x + CameraScreen.width / 3, 
-				_player.transform.position.y + CameraScreen.width / 3, 
+				_player.transform.position.x + CameraScreen.width / GetDivider(), 
+				_player.transform.position.y + CameraScreen.width / GetDivider(), 
 				transform.position.z), 
 			1f);
 	}
+
+	private int GetDivider() { 
+		if (Camera.main.aspect >= 1.7)
+		{
+				return 16;
+		}
+		else if (Camera.main.aspect >= 1.5)
+		{
+				return 3;
+		}
+		else
+		{
+				return 4;
+		}
+	} 
 }
