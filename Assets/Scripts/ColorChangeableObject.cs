f61f3fa5c667dc638c734cdb3059bb2e0004afd6
timgoeij <tim_goeijenbier@live.nl>
2017-08-30 12:56:33 +0200

    color changing

---
+++ Assets/Scripts/ColorChangeableObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeableObject : MonoBehaviour {
+
+    public enum Colors
+    {
+        Blue, 
+        Red,
+        Yellow,
+        Green
+    }
+
+    [SerializeField]
+    protected Colors colorChanges = Colors.Blue;
+
+    private Renderer colorRenderer;
+    
+    
+    // Use this for initialization
+    public virtual void Start ()
+    {
+
+        colorRenderer = GetComponent<Renderer>();
+        ChangeColor(true);
+	}
+
+    public void ChangeColor(bool startUp = false)
+    {
+        switch(colorChanges)
+        {
+            case Colors.Blue:
+                colorChanges = ApplyColors(Colors.Blue, Colors.Red, startUp);
+                colorRenderer.material.color = ApplyColor(Color.blue, Color.red, startUp);
+                break;
+            case Colors.Red:
+                colorChanges = ApplyColors(Colors.Red, Colors.Yellow, startUp);
+                colorRenderer.material.color = ApplyColor(Color.red, Color.yellow, startUp);
+                break;
+            case Colors.Yellow:
+                colorChanges = ApplyColors(Colors.Yellow, Colors.Green, startUp);
+                colorRenderer.material.color = ApplyColor(Color.yellow, Color.green, startUp);
+                break;
+            case Colors.Green:
+                colorChanges = ApplyColors(Colors.Green, Colors.Blue, startUp);
+                colorRenderer.material.color = ApplyColor(Color.green, Color.blue, startUp);
+                break;
+        }
+    }
+
+    private Color ApplyColor(Color startColor, Color changeColor, bool start)
+    {
+        return start ? startColor : changeColor;
+    }
+
+    private Colors ApplyColors(Colors colorStart, Colors colorChange, bool start)
+    {
+        return start ? colorStart : colorChange;
+    }
+    
+    public Colors GetCurrentColor()
+    {
+        return colorChanges;
+    }
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Laser : ColorChangeableObject {
+
+    private Rigidbody2D body;
+    private BoxCollider2D col;
+    private float width;
+    
+    // Use this for initialization
+	public override void Start () {
+
+        base.Start();
+
+        body = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+
+        body.gravityScale = 0;
+        body.isKinematic = true;
+        Debug.Log(body.isKinematic);
+        Debug.Log(body.gravityScale);
+
+        width = GetComponent<SpriteRenderer>().sprite.textureRect.width;
+        width /= 100;
+
+        FindObjectOfType<InputManager>().add(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2((transform.position.x - width / 2), transform.position.y),Vector2.left);
+
+        Debug.DrawRay(new Vector2((transform.position.x - width / 2), transform.position.y), Vector2.left, Color.red);
+
+        if(hit.collider != null)
+        {
+            Debug.Log(hit.collider.name);
+            if (hit.collider.CompareTag("Player"))
+            {
+                ColorChangeableObject player = hit.collider.GetComponent<ColorChangeableObject>();
+                
+
+                if(player.GetCurrentColor() == colorChanges)
+                {
+                    col.isTrigger = true;
+                }
+                else
+                {
+                    col.isTrigger = false;
+                }
+            }
+        }
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            FindObjectOfType<InputManager>().remove(this);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Debug.Log("isCollision");
+        }
+    }
+}
