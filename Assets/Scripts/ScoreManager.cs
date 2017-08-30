42338f92b7a3f4aca398305ef15ef1b25e1cb72a
Marcel Dillen <marceldillen@me.com>
2017-08-30 16:25:30 +0200

    Add timer

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	private float _totalSeconds = 0;
+
+	[SerializeField]
+	private GameObject _timeText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateTime();
+	}
+
+	void UpdateTime() {
+		_totalSeconds += Time.deltaTime;
+
+		_timeText.GetComponent<UnityEngine.UI.Text>().text = Math.Round(_totalSeconds, 1).ToString();
+ 	}
+
+}
