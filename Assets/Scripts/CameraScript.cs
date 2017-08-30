0776d5ed77d37ab8d17df66baf45eaafa857bd34
Marcel Dillen <marceldillen@me.com>
2017-08-30 13:13:48 +0200

    Add camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject _player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, new Vector3(_player.transform.position.x + CameraScreen.width / 3, _player.transform.position.y, transform.position.z), 1f);
+	}
+}
